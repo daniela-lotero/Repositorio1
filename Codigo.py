64dfde134df221f7a65873fbc44b6326ee04c596
JulyAriza <arizajuly63@gmail.com>
2023-08-02 19:42:39 -0500

    Archivos Subidos

---
+++ Codigo.py
@@ -0,0 +1,15 @@
+
+print("¡Hola, mundo!")
+
+numero1 = 10
+numero2 = 5
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
